6fb4773e0c919432a1afeead67caae39f2429311
jbratcher <jeremybratcher@gmail.com>
2019-08-19 20:14:21 -0400

    Updates

---
+++ precache-manifest.7a1e55fadaf4d93459e45690ef64ed88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe1de855fcb50b0eacb5b5b8a3ecacd",
+    "url": "/neightborhood-sim/index.html"
+  },
+  {
+    "revision": "341ed4e01d0935de9f0a",
+    "url": "/neightborhood-sim/static/css/main.eefd9873.chunk.css"
+  },
+  {
+    "revision": "6c9d077ce2405e253381",
+    "url": "/neightborhood-sim/static/js/2.779329c9.chunk.js"
+  },
+  {
+    "revision": "341ed4e01d0935de9f0a",
+    "url": "/neightborhood-sim/static/js/main.fa7334a5.chunk.js"
+  },
+  {
+    "revision": "dfbfafd72ed246ec384d",
+    "url": "/neightborhood-sim/static/js/runtime~main.4fd3411e.js"
+  }
+]);
